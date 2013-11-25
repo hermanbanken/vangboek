3396846e4aa352dcfb35cb3dc29309a0fe9794b3
Herman Banken <hermanbanken@gmail.com>
2013-11-25 14:34:32 +0100

    Fix: always publish profile

--- server/publish.js
+++ server/publish.js
@@ -1,6 +1,7 @@
 Meteor.publish(null, function () {
   return [
     Meteor.users.find({}, {fields: {
+      'profile': 1,
       'services.google.email': 1,
       'services.vangboek.email': 1,
       'services.google.picture': 1
